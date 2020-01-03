abb1b6075f5a6ca12b6a1115ef4c301193b94307
shashi8877 <shashi888777@gmail.com>
2020-01-03 12:17:16 +0530

    class SquareAreaDemo2 {

---
+++ SquareAreaDemo2.java
@@ -0,0 +1,10 @@
+class SquareAreaDemo2 {
+   public static void main (String[] args)
+   {
+       //Value specified in the program itself
+       double side = 4.5;
+       //Area of Square = side*side
+       double area = side*side; 
+       System.out.println("Area of Square is: "+area);
+   }
+}
